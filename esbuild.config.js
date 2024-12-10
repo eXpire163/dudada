5ba60fe807369580de652c1d88ada19e60a425fb
Christian Wiedemann-Gruber <christian.wiedemann@capgemini.com>
2024-12-10 21:50:51 +0100

    add server build

---
+++ esbuild.config.js
@@ -0,0 +1,17 @@
+// esbuild.config.js
+const { build } = require('esbuild');
+
+build({
+    entryPoints: ['server.js'], // Entry point of your Express app
+    outfile: 'backend/server.js',          // Output bundled file
+    platform: 'node',                   // Target Node.js environment
+    bundle: true,                       // Bundle all dependencies
+    external: ['express', 'ws'],        // External dependencies not bundled
+    minify: true,                       // Minify for production
+    sourcemap: true,                    // Generate source map for debugging
+})
+    .then(() => console.log('Server built successfully!'))
+    .catch((err) => {
+        console.error(err);
+        process.exit(1);
+    });
